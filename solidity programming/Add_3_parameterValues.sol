af6ad4ebdd6592e93ac8e5d8aed35c0182d95fa9
Remix Workspace <>
2022-08-28 20:52:08 +0530

    test message

---
+++ solidity programming/Add_3_parameterValues.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT  
+
+pragma solidity ^0.8.7;
+
+import "hardhat/console.sol";
+
+
+contract addValues{
+ 
+ 
+  function add(uint b,uint c,uint d) public pure returns(uint){
+       return b+c+d;
+
+       //add returns the sum of b+c+d 
+  }
+
+   
+}
